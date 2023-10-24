07a997bc5934d440b7f6bc4eea8102e4b400f242
shotapailodze <shotikophailodze1998@gmail.com>
2023-10-24 21:15:37 +0400

    mobile menu needs fixing and cv

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import './App.css'
+import styled, {ThemeProvider} from 'styled-components'
+import {darkTheme} from './utils/Themes'
+import Navbar from './components/Navbar/index'
+import Hero from './components/HeroSection/index'
+import Skills from './components/Skills/index'
+import { BrowserRouter as Router } from 'react-router-dom'
+import Projects from './projects/projects'
+
+const Body = styled.div`
+  background-color: ${({theme}) => theme.bg};
+  width: 100%;
+  height: 100%;
+  overflow-x: hidden;
+`
+
+const Wrapper = styled.div`
+  background: linear-gradient(
+      38.73deg,
+      rgba(204, 0, 187, 0.15) 0%,
+      rgba(201, 32, 184, 0) 50%
+    ),
+    linear-gradient(
+      141.27deg,
+      rgba(0, 70, 209, 0) 50%,
+      rgba(0, 70, 209, 0.15) 100%
+    );
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
+`;
+
+
+function App() {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Router>
+        <Navbar />
+        <Body>
+          <Hero />
+          <Wrapper>
+            <Skills />
+          </Wrapper>
+        <Projects />
+        </Body>
+      </Router>
+    </ThemeProvider>
+  )
+}
+
+export default App
